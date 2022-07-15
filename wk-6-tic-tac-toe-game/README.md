eea34db34a07dfc241b37640dac5f90266c5e76a
benjaminangafua1997 <benjaminarkutl2017@gmail.com>
2022-07-15 17:00:03 +0000

    Starting over

--- wk-6-tic-tac-toe-game/README.md
+++ wk-6-tic-tac-toe-game/README.md
@@ -16,3 +16,93 @@ Tic-Tac-Toe is basically a two man game where you try to beat your opponent by m
 4. When all 9 squares are full, and no player has 3 marks in a row, the game should end in a tie.
 5. If a player wins or the game ties, there should be a way to play again or reset the game.
 6. There should be a way to keep track of the scores between a player and the computer.
+
+
+<script> 
+// Javascript program to check whether a given 
+// tic tac toe board is valid or not 
+
+// This matrix is used to find indexes 
+// to check all possible winning triplets 
+// in board[0..8] 
+
+// Returns true if character 'c' wins. 
+// c can be either 'X' or 'O' 
+function isCWin(board, c) 
+{ 
+	let win = new Array(
+                new Array(0, 1, 2), // Check first row. 
+				new Array(3, 4, 5), // Check second Row 
+				new Array(6, 7, 8), // Check third Row 
+				new Array(0, 3, 6), // Check first column 
+				new Array(1, 4, 7), // Check second Column 
+				new Array(2, 5, 8), // Check third Column 
+				new Array(0, 4, 8), // Check first Diagonal 
+				new Array(2, 4, 6)
+            ); // Check second Diagonal 
+				
+	// Check all possible winning combinations 
+	for (let i = 0; i < 8; i++) 
+		if (board[win[i][0]] == c && 
+			board[win[i][1]] == c && 
+			board[win[i][2]] == c ) 
+			return true; 
+	return false; 
+} 
+
+// Returns true if given board is 
+// valid, else returns false 
+function isValid(board) 
+{ 
+	// Count number of 'X' and 'O' 
+	// in the given board 
+	let xCount = 0; 
+	let oCount = 0; 
+	for (let i = 0; i < 9; i++) 
+	{ 
+		if (board[i] == 'X') xCount++; 
+		if (board[i] == 'O') oCount++; 
+	} 
+
+	// Board can be valid only if either 
+	// xCount and oCount is same or count 
+	// is one more than oCount 
+	if (xCount == oCount || xCount == oCount + 1) 
+	{ 
+		// Check if 'O' is winner 
+		if (isCWin(board, 'O')) 
+		{ 
+			// Check if 'X' is also winner, 
+			// then return false 
+			if (isCWin(board, 'X')) 
+				return false; 
+
+			// Else return true xCount and 
+			// yCount are same 
+			return (xCount == oCount); 
+		} 
+
+		// If 'X' wins, then count of X 
+		// must be greater 
+		if (isCWin(board, 'X') && 
+				xCount != oCount + 1) 
+		return false; 
+
+		// If 'O' is not winner, then 
+		// return true 
+		return true; 
+	} 
+	return false; 
+} 
+
+// Driver Code 
+let board = new Array('X', 'X', 'O','O', 
+			'O', 'X','X', 'O', 'X'); 
+if(isValid(board)) 
+	document.write("Given board is valid"); 
+else
+	document.write("Given board is not valid"); 
+	
+// This code is contributed 
+// by Saurabh Jaiswal 
+</script>
--- wk-6-tic-tac-toe-game/scriptassign.js
+++ wk-6-tic-tac-toe-game/scriptassign.js
@@ -1,43 +1,52 @@
-// different players option
-var number_ofPlayer = 1;
+let number_ofPlayer = 1;
+let player_counter = 0;
 
-// incrementing after a play
-var player_counter = 0;
 
-// Player1 identification option "X" or "O"
-var person;
-var machine;
+let person;
+let machine;
 
 // Two Players identification option "X" or "O"
-var player1;
-// var player2;
+let player1;
+let player2;
 
-// Score keeping after a won
-var human_score = 0;
-var computer_score = 0;
 
-// Check for winning and stop the next player
-var winner_checker = false;
+let human_score = 0;
+let computer_score = 0;
 
-// Accessing the HTML file
-var human_call = document.getElementById("human");
-var computer_call = document.getElementById("computer");
-var container_div = document.getElementById("container");
-var selecting_btn = document.getElementById("selecting-btn");
-var back_btn = document.getElementById("backBtn");
-var optionForPlayer = document.getElementById("optionForPlayer");
-var tie_div = document.getElementById("tieStatement");
-var two_playBtn = document.getElementById("play2-btn");
-var one_playBtn = document.getElementById("play1-btn");
-var who_to_play = document.getElementById("whoToPlay");
-var board = document.querySelectorAll(".box");
-var hide_caution = document.getElementById("caution-div");
-var display_guide = document.getElementById("guide");
-var keepingScore = document.getElementById("keepingScore");
+
+let winner_checker = false;
+
+// Draw Box
+let container_div = document.getElementById("container");
+
+for (let i = 1; i <= 9; i++) {
+    let bBox = document.createElement("div")
+    bBox.setAttribute("class", "box")
+    bBox.setAttribute("id", `box${i}`)
+    bBox.setAttribute("value", `${i}`)
+    bBox.setAttribute("onclick", `humanPlayer(event)`)
+    container_div.append(bBox)
+
+}
+
+let optionForPlayer = document.getElementById("optionForPlayer");
+let selecting_btn = document.getElementById("selecting-btn");
+let keepingScore = document.getElementById("keepingScore");
+let hide_caution = document.getElementById("caution-div");
+let computer_call = document.getElementById("computer");
+let two_playBtn = document.getElementById("play2-btn");
+let one_playBtn = document.getElementById("play1-btn");
+let who_to_play = document.getElementById("whoToPlay");
+let tie_div = document.getElementById("tieStatement");
+let display_guide = document.getElementById("guide");
+let back_btn = document.getElementById("backBtn");
+let human_call = document.getElementById("human");
+let board = document.querySelectorAll(".box");
 
 // Player selection
 
 function onePlayer() {
+    let tie_div = document.getElementById("tieStatement");
     who_to_play.style.display = "none";
     selecting_btn.style.display = "block";
     number_ofPlayer = 1;
@@ -67,132 +76,91 @@ function getPlayer(choose) {
 
 
 // winning statement
+
+function checkForWinner(boardVal, innerVal) {
+    const WINNER = [
+        [1, 2, 3],
+        [1, 4, 7],
+        [1, 5, 9],
+        [2, 5, 8],
+        [3, 6, 9],
+        [3, 5, 7],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    for (let i = 0; i < 8; i++) {
+        if (boardVal[WINNER[i][0]] == innerVal &&
+            boardVal[WINNER[i][0]] == innerVal &&
+            boardVal[WINNER[i][0]] == innerVal) {
+            return true
+        }
+    }
+    return false
+}
+
 function getWinner() {
-
+    for (let i = 0; i < board.length; i++) {
+        // console.log(board[i].getAttribute("value"))
+    }
     if (board[0].innerHTML === person && board[1].innerHTML === person && board[2].innerHTML === person) {
+
+
         document.getElementById("you_win").innerHTML = "Human Wins";
-        human_score++;
-        human_call.innerHTML = human_score;
+        human_score += 1;
+        // human_call.innerHTML = human_score;
         winner_checker = true;
 
-
-    } else if (board[0].innerHTML === machine && board[1].innerHTML === machine && board[2].innerHTML === machine) {
-        document.getElementById("computerWin").innerHTML = "Woo! Computer Win";
-        computer_score++;
-        computer_call.innerHTML = computer_score;
-
-    } else if (board[3].innerHTML === person && board[4].innerHTML === person && board[5].innerHTML === person) {
-        document.getElementById("you_win").innerHTML = "Human Wins";
-        human_score++;
-        human_call.innerHTML = human_score;
-        winner_checker = true;
-
-    } else if (board[3].innerHTML === machine && board[4].innerHTML === machine && board[5].innerHTML === machine) {
-        document.getElementById("computerWin").innerHTML = "Woo! Computer Win";
-        computer_score++;
-        computer_call.innerHTML = computer_score;
-
-
-    } else if (board[6].innerHTML === person && board[7].innerHTML === person && board[8].innerHTML === person) {
-        document.getElementById("you_win").innerHTML = "Human Wins";
-        human_score++;
-        human_call.innerHTML = human_score;
-        winner_checker = true;
-
-    } else if (board[6].innerHTML === machine && board[7].innerHTML === machine && board[8].innerHTML === machine) {
-        document.getElementById("computerWin").innerHTML = "Woo! Computer Win";
-        computer_score++;
-        computer_call.innerHTML = computer_score;
-
-    } else if (board[0].innerHTML === person && board[3].innerHTML === person && board[6].innerHTML === person) {
-        document.getElementById("you_win").innerHTML = "Human Wins";
-        human_score++;
-        human_call.innerHTML = human_score;
-        winner_checker = true;
-
-    } else if (board[0].innerHTML === machine && board[3].innerHTML === machine && board[6].innerHTML === machine) {
-        document.getElementById("computerWin").innerHTML = "Woo! Computer Win";
-        computer_score++;
-        computer_call.innerHTML = computer_score;
-
-    } else if (board[1].innerHTML === person && board[4].innerHTML === person && board[7].innerHTML === person) {
-        document.getElementById("you_win").innerHTML = "Human Wins";
-        human_score++;
-        human_call.innerHTML = human_score;
-        winner_checker = true;
-
-    } else if (board[1].innerHTML === machine && board[4].innerHTML === machine && board[7].innerHTML === machine) {
-        document.getElementById("computerWin").innerHTML = "Woo! Computer Win";
-        // container_div.style.display = "none";
-        computer_score++;
-        computer_call.innerHTML = computer_score;
-
-    } else if (board[2].innerHTML === person && board[5].innerHTML === person && board[8].innerHTML === person) {
-        document.getElementById("you_win").innerHTML = "Human Wins";
-        human_score++;
-        human_call.innerHTML = human_score;
-        winner_checker = true;
-
-    } else if (board[2].innerHTML === machine && board[5].innerHTML === machine && board[8].innerHTML === machine) {
-        document.getElementById("computerWin").innerHTML = "Woo! Computer Win";
-        // container_div.style.display = "none";
-        computer_score++;
-        computer_call.innerHTML = computer_score;
-
-    } else if (board[2].innerHTML === person && board[4].innerHTML === person && board[6].innerHTML === person) {
-        document.getElementById("you_win").innerHTML = "Human Wins";
-        human_score++;
-        human_call.innerHTML = human_score;
-        winner_checker = true;
-
-    } else if (board[2].innerHTML === machine && board[4].innerHTML === machine && board[6].innerHTML === machine) {
-        document.getElementById("computerWin").innerHTML = "Woo! Computer Win";
-        // container_div.style.display = "none";
-        computer_score++;
-        computer_call.innerHTML = computer_score;
-
-    } else if (board[0].innerHTML === person && board[4].innerHTML === person && board[8].innerHTML === person) {
-        document.getElementById("you_win").innerHTML = "Human Wins";
-        human_score++;
-        human_call.innerHTML = human_score;
-        winner_checker = true;
-
-    } else if (board[0].innerHTML === machine && board[4].innerHTML === machine && board[8].innerHTML === machine) {
-        document.getElementById("computerWin").innerHTML = "Woo! Computer Win";
-        // container_div.style.display = "none";
-        computer_score++;
-        computer_call.innerHTML = computer_score;
-    } else {
-        let tie = 0;
-        if (tie < board.length) {
-            for (let i = 0; i < board.length; i++) {
-                if (board[i].innerHTML != "")
-                    tie += 1;
-            }
-        }
-        if (tie === 9) {
-            tie_div.style.display = "block";
-            container_div.style.display = "none";
-        }
 
     }
 
-}
 
-// Human player
-function humanplayer(e) {
+
+    // console.log(board)
+    // else if (board[0].innerHTML === machine && board[1].innerHTML === machine && board[2].innerHTML === machine) {
+    //         document.getElementById("computerWin").innerHTML = "Woo! Computer Win";
+    //         computer_call.innerHTML = computer_score;
+    //         computer_score++;
+
+    //     } else if (board[3].innerHTML === person && board[4].innerHTML === person && board[5].innerHTML === person) {
+    //         document.getElementById("you_win").innerHTML = "Human Wins";
+    //         human_call.innerHTML = human_score;
+    //         winner_checker = true;
+    //         human_score++;}else {
+
+    //         let tie = 0;
+    //         if (tie < board.length) {
+    //             for (let i = 0; i < board.length; i++) {
+    //                 if (board[i].innerHTML != "")
+    //                     tie += 1;
+    //             }
+    //         }
+    //         if (tie === 9) {
+    //             tie_div.style.display = "block";
+    //             container_div.style.display = "none";
+    //         }
+
+    //     }
+
+}
+let clickedValue = []
+    // Human player
+function humanPlayer(e) {
+    console.log(e.target.value)
+
     // condition for playing with computer
     if (number_ofPlayer < 2 && winner_checker === false) {
         if (e.target.innerHTML === "") {
-            // console.log(e.target)
             player_counter++;
             e.target.innerHTML = person;
-            getWinner();
+            // getWinner();
             computerPlayer();
         }
         // Two player condition without computer involvement
     } else {
-        var box = document.getElementById(e.target.id).innerHTML = person;
+        let box = document.getElementById(e.target.id)
+
+
+        box.innerHTML = person;
         if (box.innerHTML === "") {
             player_counter++;
         } else {
@@ -209,27 +177,30 @@ function humanplayer(e) {
 //  Computer player function
 function computerPlayer() {
     const computer = Math.floor(Math.random() * 8) + 1;
-    if (document.getElementById("box" + computer).innerHTML === "") {
-        document.getElementById("box" + computer).innerHTML = machine;
+    // console.log(computer)
+    if (document.getElementById(`box${computer}`).innerHTML === "") {
+
+        document.getElementById(`box${computer}`).innerHTML = machine;
         getWinner();
         player_counter++;
     } else {
         // avoiding the computer generating number more  than the board holds
         if (player_counter < board.length) {
-            // requesive function
+            // request function
             computerPlayer();
         }
     }
 }
 
-let play_again_btn = document.querySelector("#playagainbtn")
+let play_again_btn = document.querySelector("#playAgainBtn")
 play_again_btn.addEventListener("click", playAgain);
 
 function playAgain() {
     for (let i = 0; i < board.length; i++) {
         if (board[i].innerHTML != "" && player_counter == 9) {
             board[i].innerHTML = ""
-                // computerPlayer();
+        } else {
+            computerPlayer()
         }
 
     }
